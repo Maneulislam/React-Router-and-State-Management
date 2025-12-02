1dbf1f2c10e8b1509397f79f3710767b6a7cffeb
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2025-12-02 22:37:32 +0600

    project setup and basic routing

---
+++ react-router-concept/src/component/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+
+
+const Navbar = () => {
+    return (
+        <div>
+            This is Navbar
+
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ react-router-concept/src/main.jsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './component/Root/Root.jsx'
+import Home from './component/Home/Home.jsx'
+import Mobiles from './component/Mobiles/Mobiles.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: "mobiles", Component: Mobiles },
+      { path: "app", Component: App },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+
+    <RouterProvider router={router} />
+
+  </StrictMode>,
+)
